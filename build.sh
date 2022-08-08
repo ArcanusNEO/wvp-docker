98120fa172e0c4e8348574c042c3fec726246126
ArcanusNEO <lucasding@foxmail.com>
2022-08-08 15:07:25 +0800

    .

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -ex
 docker build -f Dockerfile.zl -t lucas/zlmediakit:0.1.0 .
 docker pull mysql:latest
 docker build -f Dockerfile.wvp -t lucas/wvp:0.1.0 .
-docker run -itd --restart=always --name mysql-wvp --ip '172.17.0.254' -e MYSQL_ROOT_PASSWORD='123465' mysql:latest
-docker run -itd --restart=always --name zlmediakit --ip '172.17.0.253' lucas/zlmediakit:0.1.0
-docker run -itd --restart=always --name wvp -e MYSQL_HOST='172.17.0.254' -e MYSQL_PWD='123465' -e ZLM_HOST='172.17.0.253' -p 5060:5060 -p 18080:18080 lucas/wvp:0.1.0
+
+docker run -itd --restart=always --name mysql-wvp -e MYSQL_ROOT_PASSWORD=123465 mysql:latest
+docker run -itd --restart=always --name zlmediakit lucas/zlmediakit:0.1.0
+docker run -itd --restart=always --name wvp -e MYSQL_HOST=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mysql-wvp) -e MYSQL_PWD=123465 -e ZLM_HOST=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' zlmediakit) -p 5060:5060 -p 18080:18080 lucas/wvp:0.1.0
