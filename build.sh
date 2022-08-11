43ab772bb9ed75bd5dd32885b0d517ef62727ede
ArcanusNEO <lucasding@foxmail.com>
2022-08-11 10:39:55 +0800

    .

--- build.sh
+++ build.sh
@@ -9,18 +9,20 @@ docker build -f Dockerfile.zl -t lucas/zlmediakit:0.1.0 .
 docker build -f Dockerfile.wvp -t lucas/wvp:0.1.0 .
 docker run -d --restart=always --name zlmediakit lucas/zlmediakit:0.1.0
 
+echo "MySQL starting"
 while
   MYSQL_STATUS=$(docker inspect --format "{{.State.Health.Status}}" mysql-wvp)
-  [ $MYSQL_STATUS != "healthy" ]
+  [ "$MYSQL_STATUS"_ != "healthy"_ ]
 do
-  if [ $STATUS == "unhealthy" ]; then
-    echo "MySQL failed to start!"
+  if [ "$MYSQL_STATUS"_ == "unhealthy"_ ]; then
+    echo "MySQL failed to start"
     exit -1
   fi
   printf .
   sleep 1
 done
-sleep 1 && echo ""
+sleep 1 && echo "" && echo "MySQL successfully started"
+
 echo 'CREATE DATABASE IF NOT EXISTS wvp DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;' | docker exec -i mysql-wvp mysql -ABq -uroot -p$MYSQL_ROOT_PASSWORD
 curl -fsSL https://gitee.com/pan648540858/wvp-GB28181-pro/raw/wvp-28181-2.0/sql/mysql.sql | sed -E -e 's/wvp2/wvp/g' -e 's/\s\+\s00:00/UTC/g' | docker exec -i mysql-wvp mysql -ABq -uroot -p$MYSQL_ROOT_PASSWORD -Dwvp
 
